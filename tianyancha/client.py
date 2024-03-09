6fd0cc7de1ca338ee1e4b57506796c90f0025302
WeiJie Ou <ouweijie123@outlook.com>
2024-03-09 18:58:08 +0800

    init repo

--- tianyancha/client.py
+++ tianyancha/client.py
@@ -5,8 +5,7 @@
 # @description --
 import json
 import logging
-
-from db.models import Company, CompanyShareholder, CompanyManager
+from db.models import Company
 from tianyancha import *
 from urllib.parse import quote
 from util.httpclient import Request
@@ -57,70 +56,13 @@ class TycClient:
             # 公司主体基本信息
             self.EntityHelper.__basic_info__(company, company_entity)
 
-            def is_equal(b_and_a):
-                return company.get('id') == b_and_a.get('graphId')
-
-            try:
-                # 公司主体融资阶段、竟品信息
-                brand_and_agency = filter(is_equal, self.brand_and_agencies).__next__()
-                self.EntityHelper.__another_info__(brand_and_agency, company_entity)
-            except:
-                logging.warning('竟品信息获取失败！')
-                pass
-            """ 公司详情 """
-            detail_resp = Request(TycPortraitApi.format(eid=company.get("id")), headers=REQUEST_HEADERS).data
-            if detail_resp:
-                company_portrait = json.loads(detail_resp)
-                # 公司详情补充信息
-                if company_portrait.get("state") == 'ok':
-                    self.EntityHelper.__additional__(company_portrait.get("data", {}), company_entity)
-
-            shareholder_request_body = {
-                "graphId": company.get("id"),
-                "hkVersion": 1,
-                "typeList": {
-                    "shareHolder": {
-                        "pageNum": 1,
-                        "pageSize": 20,
-                        "required": "true"
-                    }
-                }
-            }
-            """ 股东信息 """
-            shareholder_resp = Request(TycShareholderPostApi, method='post', json=shareholder_request_body, headers=REQUEST_HEADERS).data
-            if shareholder_resp:
-                company_shareholder = json.loads(shareholder_resp)
-                # 公司详情补充信息
-                if company_shareholder.get("state") == 'ok':
-                    self.EntityHelper.__shareholder__(company_shareholder.get("data", {}).get("shareHolder", {}), company_entity)
-
-            manager_request_body = {
-                "graphId": company.get("id"),
-                "hkVersion": 1,
-                "typeList": {
-                    "companyStaff": {
-                        "pageNum": 1,
-                        "pageSize": 20,
-                        "required": "true"
-                    }
-                }
-            }
-            """ 高管信息 """
-            manager_resp = Request(TycEnterpriseManagerPostApi, method='post', json=manager_request_body, headers=REQUEST_HEADERS).data
-            if manager_resp:
-                company_manager = json.loads(manager_resp)
-                # 公司详情补充信息
-                if company_manager.get("state") == 'ok':
-                    self.EntityHelper.__company_manager__(company_manager.get("data", {}).get("companyStaff", {}), company_entity)
-            self.companies.append(company_entity)
-
     class EntityHelper:
         @staticmethod
         def __basic_info__(src: dict, target: Company):
             # 公司外部系统ID
             target.id = src.get('id', '-')
             # 公司名称
-            target.name = src.get('name', '-').replace('<em>', '').replace('</em>', '')
+            target.name = src.get('name', '-').replace("</em>","").replace("<em>","")
             # 公司简称
             target.short_name = src.get('alias', '-')
             # 公司法人
@@ -172,70 +114,4 @@ class TycClient:
             target.tags = src.get('labelListV2', [])
             # 公司行业分类
             target.industry = src.get('categoryStr', '-')
-
-        @staticmethod
-        def __another_info__(brand_and_agency: dict, company: Company):
-            # 公司融资轮次
-            company.financing_round = brand_and_agency.get("round", "未知")
-            # 公司竟品信息
-            company.competitions = brand_and_agency.get("jingpinName", [])
-            # 公司logo
-            company.logo = brand_and_agency.get("logo")
-            # 公司简介
-            company.company_desc = brand_and_agency.get("intro")
-
-        @staticmethod
-        def __additional__(src: dict, company: Company):
-            # 公司英文名
-            company.english_name = src.get('property3')
-            if not company.english_name:
-                company.english_name = src.get('nameEn', '-')
-            # 公司注册机构
-            company.register_institute = src.get('regInstitute', '-')
-            # 公司网站地址集
-            company.websites = src.get('websiteList', '-')
-            # 公司实缴资本
-            company.actual_capital = src.get('actualCapital', '缺省')
-            # 公司曾用名
-            company.used_name = src.get('historyNames', '-')
-            # 公司员工人数
-            company.staffs = src.get('socialStaffNum', None)
-            if not company.staffs:
-                company.staffs = src.get('staffNum', 1)
-            # 公司纳税地址
-            company.tax_address = src.get('taxAddress', None)
-            if not company.tax_address:
-                company.tax_address = src.get('regLocation', '-')
-            # 公司纳税银行
-            company.taxpayer_bank = src.get('taxBankName', '-')
-            # 公司涉足领域标签
-            company.portraits = src.get('portray', [])
-            if not company.logo:
-                company.logo = src.get('logo')
-            if not company.company_desc:
-                company.company_desc = src.get('baseInfo', '-')
-
-        @staticmethod
-        def __shareholder__(src: dict, company: Company):
-            holder_list = src.get("holderList", [])
-            for holder in holder_list:
-                if holder:
-                    shareholder = CompanyShareholder()
-                    shareholder.name = holder.get("name")
-                    shareholder.alias = holder.get("alias")
-                    shareholder.avatar = holder.get("logo")
-                    shareholder.control_ratio = holder.get("proportion")
-                    shareholder.tags = [tag.get("name") for tag in holder.get("tagList", [])]
-                    company.shareholders.append(shareholder)
-
-        @staticmethod
-        def __company_manager__(src: dict, company: Company):
-            manager_list = src.get("result", [])
-            manager_type = src.get("staffTitle", "-")
-            for manager in manager_list:
-                company_manager = CompanyManager()
-                company_manager.manager_type = manager_type
-                company_manager.name = manager.get("name", "-")
-                company_manager.titles = manager.get("typeJoin", [])
-                company.managers.append(company_manager)
 
--- util/httpclient.py
+++ util/httpclient.py
@@ -6,10 +6,7 @@
 :desc: http请求工具类
 """
 import logging
-
 import requests
-
-from config import GLOBAL_PROXY, PROXY_POOL_URL
 
 
 class Request:
@@ -19,39 +16,23 @@ class Request:
         self.params = params
         self.data = None
         self.method = method
-        if self.method == 'post':
+        if self.method == "post":
             self.post(**kwargs)
         else:
             self.get(**kwargs)
 
     def get(self, **kwargs):
-        p = proxy() if GLOBAL_PROXY and self.proxy else None
-        resp = requests.get(self.url, params=self.params, verify=False, proxies=p, **kwargs)
+        resp = requests.get(
+            self.url, params=self.params, verify=False, proxies=None, **kwargs
+        )
         if resp and resp.status_code == 200:
             self.data = resp.text
         else:
             logging.warning(resp)
 
     def post(self, **kwargs):
-        p = proxy() if GLOBAL_PROXY and self.proxy else None
-        resp = requests.post(self.url, verify=False, proxies=p, **kwargs)
+        resp = requests.post(self.url, verify=False, proxies=None, **kwargs)
         if resp and resp.status_code == 200:
             self.data = resp.text
         else:
             logging.warning(resp)
-
-
-def proxy():
-    import json
-    r = requests.get(f"{PROXY_POOL_URL}/get")
-    if r and r.status_code == 200:
-        p = json.loads(r.text)
-        if p['https']:
-            return {"http": "https://%s" % p.get("proxy")}
-        else:
-            return {"http": "http://%s" % p.get("proxy")}
-
-
-if __name__ == '__main__':
-    print(proxy())
-    
